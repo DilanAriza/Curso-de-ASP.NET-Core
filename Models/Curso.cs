7cb65736a94ee8dc0f63232966a59462fe294bf4
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-02 14:00:00 -0500

    Añadiendo la configuración de los modelos para poder obligar al modelo del curso a tener nombre

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Curso_de_ASP.NET_Core.Models
 {
     public class Curso:ObjetoEscuelaBase
     {
+        [Required]
+        public override string Nombre {set;get;}
         public TiposJornada Jornada { get; set; }
         public List<Asignatura> Asignaturas{ get; set; }
         public List<Alumno> Alumnos{ get; set; }
--- Models/ObjetoEscuelaBase.cs
+++ Models/ObjetoEscuelaBase.cs
@@ -5,7 +5,7 @@ namespace Curso_de_ASP.NET_Core.Models
     public abstract class ObjetoEscuelaBase
     {
         public string Id { get; set; }
-        public string Nombre { get; set; }
+        public virtual string Nombre { get; set; }
 
         public ObjetoEscuelaBase()
         {
