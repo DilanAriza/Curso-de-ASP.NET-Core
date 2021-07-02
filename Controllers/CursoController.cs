592835d46ef2ad4ad1d52429b70cd5b4ca2f7309
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-02 13:59:22 -0500

    Agregando validación de formulario y validación de datos en el controlador del curso

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -30,6 +30,35 @@ namespace Curso_de_ASP.NET_Core.Controllers
 
             return View("MultiCurso", _context.Cursos.ToList()); 
         }
+
+        public IActionResult Create(){
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Curso curso){
+            ViewBag.Fecha = DateTime.Now;
+
+            if(ModelState.IsValid)
+            {
+                var escuela = _context.Escuelas.FirstOrDefault();
+                curso.EscuelaId = escuela.Id;
+
+                _context.Cursos.Add(curso);
+                _context.SaveChanges();
+
+                ViewBag.mensaje = "Curso creado";
+                
+                return View("Index", curso);
+            }
+            else
+            {
+                return View(curso);
+            } 
+        }
+
         private EscuelaContext _context;
         public CursoController(EscuelaContext context)
         {
