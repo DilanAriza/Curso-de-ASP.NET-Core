acfe93b1385df543d34cc267d1ea04070f93003e
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-04 19:43:38 -0500

    Refactorizando las rutas y agregando la ruta GET de create y la POST también

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -8,36 +8,80 @@ namespace Curso_de_ASP.NET_Core.Controllers
 {
     public class AsignaturaController: Controller
     {
+        #region Variables
+        private EscuelaContext _context;
+
+        #endregion
+        
+        #region Constructor
+        public AsignaturaController(EscuelaContext context)
+        {
+           _context = context; 
+        }
+
+        #endregion
+
+        // Controllers
+        #region Controllers
+
         [Route("asignatura/")]
-        [Route("asignatura/{id}")]
+        [HttpGet]
         public IActionResult Index(string id)
         {
+            ViewBag.Fecha = DateTime.Now;
+            return View("MultiAsignatura", _context.Asignaturas.ToList());
+        }
+
+        
+        [Route("asignatura/{id}")]
+        [HttpGet]
+        public IActionResult GetOne(string id)
+        {
+            ViewBag.Fecha = DateTime.Now;
             if(!String.IsNullOrWhiteSpace(id))
             {
                 var asignatura = from asig in _context.Asignaturas
                                                 where  asig.Id == id
                                                 select asig;
 
-                return View(asignatura.SingleOrDefault());
+                return View("Index", asignatura.SingleOrDefault());
             }
             else 
             {
-                return View("MultiAsignatura", _context.Asignaturas.ToList()); 
+                return RedirectToAction("Index");
             }
         }
 
-        public IActionResult MultiAsignatura(){
+        // [GET(/create)] - create new asignatura
+        [Route("asignatura/create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Fecha = DateTime.Now;
+            return View();
+        }
 
-            ViewBag.CosaDinamica = "La monja";
+        [HttpPost]
+        public IActionResult Create(Asignatura asignaruta){
             ViewBag.Fecha = DateTime.Now;
 
-            return View("MultiAsignatura", _context.Asignaturas.ToList()); 
+            if(ModelState.IsValid)
+            {
+                var curso = _context.Cursos.FirstOrDefault();
+                asignaruta.CursoId = curso.Id;
+
+                _context.Asignaturas.Add(asignaruta);
+                _context.SaveChanges();
+
+                ViewBag.mensaje = "Asignatura creada";
+
+                return View("MultiAsignatura", _context.Asignaturas.ToList());
+            }
+            else
+            {
+                return View(asignaruta);
+            }
         }
-    
-        private EscuelaContext _context;
-        public AsignaturaController(EscuelaContext context)
-        {
-           _context = context; 
-        }
+        #endregion
     }
 }
