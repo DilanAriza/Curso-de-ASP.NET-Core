a0f25ff6cdeb3142aeb5060b4f2eca0327c4cba1
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-05 23:02:21 -0500

    Refactorizando código, creando la funcionalidad de crear alumno y de eliminar

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -8,32 +8,102 @@ namespace Curso_de_ASP.NET_Core.Controllers
 {
     public class AlumnoController: Controller
     {
-        [Route("alumno/")]
-        [Route("alumno/{id}")]
-        public IActionResult Index(string id)
-        {
-            if(!String.IsNullOrWhiteSpace(id))
-            {
-                var alumno = from alum in _context.Alumnos
-                                                where  alum.Id == id
-                                                select alum;
-                return View(alumno.SingleOrDefault());
-            }
-            else 
-            {
-                return View("MultiAlumno",  _context.Alumnos.ToList()); 
-            }
-        }
-        public IActionResult MultiAlumno(){
-            ViewBag.CosaDinamica = "La monja";
-            ViewBag.Fecha = DateTime.Now;
-
-            return View("MultiAlumno", _context.Alumnos.ToList()); 
-        }
+        #region Variables
         private EscuelaContext _context;
+
+        #endregion
+        
+        #region Constructor
         public AlumnoController(EscuelaContext context)
         {
            _context = context; 
         }
+
+        #endregion
+
+        // Controllers
+        #region Controllers
+
+        // [GET] - index
+        [Route("alumno/")]
+        [HttpGet]
+        public IActionResult Index(string id)
+        {
+            ViewBag.Fecha = DateTime.Now;
+            return View("MultiAlumno",  _context.Alumnos.ToList()); 
+        }
+
+        // [GET({id})] - index
+        [Route("alumno/{id}")]
+        [HttpGet]
+        public IActionResult GetOne(string id)
+        {
+            ViewBag.Fecha = DateTime.Now;
+            if(!String.IsNullOrWhiteSpace(id))
+            {
+                var alumno = from alum in _context.Alumnos
+                                            where alum.Id == 
+                                            id
+                                            select alum;
+                return View("Index", alumno.SingleOrDefault());
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // [GET(/create)] - index
+        [Route("alumno/create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Fecha = DateTime.Now;
+            return View();
+        }
+
+        // [POST(/create)] - index
+        [HttpPost]
+        public IActionResult Create(Alumno alumno)
+        {
+            ViewBag.Fecha = DateTime.Now;
+            if(ModelState.IsValid)
+            {
+                var curso = _context.Cursos.FirstOrDefault();
+                alumno.CursoId = curso.Id;
+
+                _context.Alumnos.Add(alumno);
+                _context.SaveChanges();
+                
+                ViewBag.mensaje = "Alumno Creado";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(alumno);
+            }
+        }
+
+        // [GET(/delete/{Id})] - Delete Alumno
+        [Route("alumno/delete/{id}")]
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            ViewBag.Fecha = DateTime.Now;
+            if(!String.IsNullOrWhiteSpace(id))
+            {
+                var alumno = _context.Alumnos.FirstOrDefault(a => a.Id == id);
+                if(alumno != null)
+                {
+                    _context.Alumnos.Remove(alumno);
+                    _context.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
     }
 }
