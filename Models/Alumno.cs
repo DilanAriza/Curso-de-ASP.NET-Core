2f2a6a09ab0ef86b24e5ec7a911cd52beb11aacd
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-16 21:39:45 -0500

    Agregando validación para el modelo de alumno

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Curso_de_ASP.NET_Core.Models
 {
     public class Alumno: ObjetoEscuelaBase
     {
+        
+        [Required(ErrorMessage ="El nombre del alumno es requerido")]
+        [MinLength(10, ErrorMessage ="El nombre del alumno no puede ser menor a 10")]
+        [Display(Prompt="Nombre del alumno", Name ="Nombre alumno")]
+        public override string Nombre {get; set;}
         public string CursoId { get; set; }
         public Curso Curso { get; set; }
         public List<Evaluación> Evaluaciones { get; set; }
